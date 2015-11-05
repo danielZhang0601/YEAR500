ee4d0a30fa91f7fb6981ed29c785e4459ee015fb
danielZhang0601 <daniel.zhang@wolaidai.com>
2015-11-05 15:40:11 +0800

    changed main page

--- app/src/main/java/com/wolaidai/year500/activities/MainActivity.java
+++ app/src/main/java/com/wolaidai/year500/activities/MainActivity.java
@@ -4,12 +4,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.wolaidai.year500.R;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
-
-    private Button btn_main_mine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +17,20 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         findViewById(R.id.iv_activity_back).setVisibility(View.GONE);
         ((TextView) findViewById(R.id.tv_activity_title)).setText(R.string.main_title);
         // init view
-        btn_main_mine = (Button) findViewById(R.id.btn_main_mine);
-
-        // init listener
-        btn_main_mine.setOnClickListener(this);
+        findViewById(R.id.btn_main_learn).setOnClickListener(this);
+        findViewById(R.id.btn_main_mine).setOnClickListener(this);
+        findViewById(R.id.btn_main_service).setOnClickListener(this);
+        findViewById(R.id.btn_main_show).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.btn_main_learn:
+            case R.id.btn_main_service:
+            case R.id.btn_main_show:
+                Toast.makeText(activityThis, R.string.to_be_continued, Toast.LENGTH_SHORT).show();
+                break;
             case R.id.btn_main_mine:
                 MineActivity.launch(v.getContext(), null);
                 break;
